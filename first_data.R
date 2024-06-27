3b150c002b77864b42afb23e901d0240fe65e252
Buchanan Lindsey <buchananlindsey@Joshuas-MacBook-Pro.local>
2024-06-27 14:22:09 -0500

    street name cleaning

--- first_data.R
+++ first_data.R
@@ -48,9 +48,6 @@ ACS_2022_vars <- read_xlsx("data/ACS2022_Table_Shells.xlsx")
 acs_2022 <- load_variables(year = 2022, dataset = "acs1", cache = TRUE)
 
 
-
-
-
 #create dataset with several different variavles from ACS
 first_acs <- get_acs(geography = "tract",
                  state = "TN",
@@ -137,12 +134,6 @@ tm_shape(pivot_first_acs) +
 # lat_longs <- lender_info %>%
 #   geocode(street = "Street", city = "City", postalcode = "Postal Code", state = "State", , country = "Country", method = 'osm', lat = latitude , long = longitude)
 
-#create address column for lender_info
-total_info <- total_info %>% 
-  mutate(full_address = paste0(Street,", ",City)) %>% 
-  mutate(full_address = paste0(full_address,", ",State)) %>% 
-  mutate(full_address = paste0(full_address," ",postal_code))
-
 #Create Loan Type Column: displays Title, Payday, Flex, or combination of the three depending on if the loan location belongs to one or many of the initial datasets
 total_info <- total_info %>% 
   mutate(loan_type = case_when(
@@ -155,13 +146,112 @@ total_info <- total_info %>%
     Street %in% title_info$Street ~ "Title",
     TRUE ~ NA_character_))
 
+# clean STREET column
+# problems: format: FIRST LINE (PROBLEM) SECOND LINE (SOLUTION)
+
+# 1203A Scottsville Highway, Lafayette, TN 37083 <- 
+# 1203A Scottsville Road, Lafayette, TN 37083, ROAD NOT HIGHWAY THANK YOU WRONG DATA!!!!
+
+# 811 N.W. Broad Street, Murfreesboro, TN 37129
+# 811 NW Broad Street, Murfreesboro, TN 37129, NO PERIODS IN DIRECTIONS
+# BUT GEO DOESNT CARE ABOUT PERDIOS WHEN THERE IS ONLY ONE DIRECTION EG: N. VS N
+
+# 101 Reed Street, Portland, TN 37148
+# ???
+
+# 321 Dexter Woods Road Suite 3, Waynesboro, TN 38485
+# 321 Dexter L Woods Memorial Blvd, Waynesboro, TN 38485, WRONG ADDRESS AWESOME!
+
+# 1600 Carmack Blvd. Ste A, Columbia, TN 38401
+# 1600 Carmack Blvd., Columbia, TN 38401, NO SUITE
+### SUITE ABBR VARIATIONS: Suite, SUITE, Ste, STE, STE., Suites ###
+
+# 214 Gallatin Pike South, Madison, TN 37115
+# ???
+
+# 1640 Hwy. 46 South, Dickson, TN 37055
+# ???
+
+# 909 New Hwy 68, Sweetwater, TN 37874
+# ???
+
+# 19798 Alberta Street B-8, Oneida, TN 37841
+# 19798 Alberta Street, Oneida, TN 37841, NO STRANGE LETTERS/SYMBOL AFTER STREET
+
+# 6729 Cumberland Gap Pkwy.#2, Harrogate, TN 37752
+# 6729 Cumberland Gap Parkway, Harrogate, TN 37752, NO NUMBERS/SYMBOL AFTER PKWY
+
+#16952A Rankin Ave, Dunlap, TN 37327
+#16952 Rankin Ave, Dunlap, TN 37327, NO LETTER AFTER STREET NUMBER
 
 
-  
-  
+# 718 Gallatin Road, Nashville, TN 37206
+# 718 Gallatin Ave, Nashville, TN 37206
 
-# Use the geocode function to get latitude and longitude
-# lat_long <- geo(address = lender_info$full_address, method = 'osm')
+# 1641 N. Memorial Blvd., Murfreesboro, TN 37129
+# 1641 N. Memorial Blvd, Murfreesboro, TN 37129, NO PERIOD AFTER BLVD
+
+# 1016 Gallatin Pike South, Madison, TN 37115
+# 1016 Gallatin Pike, Madison, TN 37115, GEO DIDNT LIKE SOUTH IN THIS ADDRESS
+
+# 310 Kimball Crossing Drive Unit #5, Kimball, TN 37347
+# 310 Kimball Crossing Drive, Kimball, TN 37347, NO "UNIT"/"UNITS"
+
+# 113 Clairborne Lane, Jacksboro, TN 37757
+# ???
+ 
+# 151 FORKS OF THE RIVER PKWY, SEVIERVILLE, TN 37862-3457
+# 151 FORKS OF THE RIVER PKWY, SEVIERVILLE, TN 37862, NO ZIP GREATER THAN 5 DIGITS
+
+# 3397 Main St, Jasper (Kimball), TN 37347
+# 3397 Main St, Jasper, TN 37347, NO PARENTHESES
+
 # 
+# 
+
+# heres the sample data
+test_coords <- read_csv("test_coords") %>% 
+  filter(is.na(lat) | is.na(long))
+
+geo(address = '909 NW Broad St, Murfreesboro, TN 37130', method = 'osm') %>% print()
+
+
+
+#create address column for lender_info
+total_info <- total_info %>% 
+  mutate(full_address = paste0(Street,", ",City)) %>% 
+  mutate(full_address = paste0(full_address,", ",State)) %>% 
+  mutate(full_address = paste0(full_address," ",postal_code))
+
+# Create small test dataset to see which address cannot convert to coordinates
+total_info_test <- total_info %>% 
+  head(275)
+#lat_long <- geo(address = total_info_test$full_address, method = 'osm')
+
+#write csv for test file so we do not waste time
+write.csv(lat_long, file = "test_coords", row.names = TRUE)
+
+#test cleaning address for coord reading
+test_cleaning2 <- test %>% 
+  filter(is.na(lat) | is.na(long)) %>% 
+  mutate(address = gsub(" N.", " North ", address, fixed = TRUE)) %>%  #replace N. with North
+  mutate(address = gsub(" N ", " North ", address, fixed = TRUE)) %>% #replace " N " with North
+  mutate(address = gsub(" S.", " South ", address, fixed = TRUE)) %>%  #replace S. with South
+  mutate(address = gsub(" E.", " East ", address, fixed = TRUE)) %>%   #replace E. with East
+  mutate(address = gsub(" E ", " East ", address, fixed = TRUE)) %>% #replace E with East
+  mutate(address = gsub(" W.|W.", " West ", address)) %>%  #replace W. with West
+  mutate(address = gsub("Hwy|HWY ", "Highway ", address)) %>%  #replace Hwy and HWY with Highway
+  mutate(address = gsub("Unit [^,]+|Unit \\d+", " ", address)) %>% #get rid of unit
+  mutate(address = gsub("Suite \\d+|Ste \\d+|Ste. \\d+|STE \\d+|Ste.|STE.|Suite [^,]+|Ste [^,]+", "", address))  %>% #get rid of suits
+  mutate(address = gsub(" ,", ",", address)) %>% #Handle double space before comma
+  mutate(address = gsub("  ", " ", address))  # Handle any double spaces
+
+
+lat_long2 <- geo(address = test_cleaning$address, method = 'osm')
+test_cleaning2 <- lat_long2 %>% 
+  filter(is.na(lat) | is.na(long))
 # missing_lat_long <- lat_long %>% 
-#   filter(is.na(lat) | is.na(long))
+test_cleaning %>% filter(is.na(lat) | is.na(long)) %>% print(n = 112)
+
+
+
